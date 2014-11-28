5eeef849fe001fc428ea42419508ac5bc6d5a149
Luca Tronchin <ltronky@gmail.com>
2014-11-29 00:26:11 +0100

    Init BSP Scala project

---
+++ build.sbt
@@ -0,0 +1,34 @@
+name := """TesiBSPScala"""
+
+val akkaVersion = "2.3.6"
+
+fork in run := true
+
+version := "0.1"
+
+scalaVersion := "2.11.2"
+
+resolvers += Resolver.sonatypeRepo("public")
+
+scalacOptions in Compile ++= Seq("-encoding", "UTF-8", "-target:jvm-1.7", "-deprecation", "-feature", "-unchecked", "-Xlog-reflective-calls", "-Xlint")
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+  "com.typesafe.akka" %% "akka-contrib" % akkaVersion,
+  "com.github.scopt" %% "scopt" % "3.2.0",
+  "org.fusesource" % "sigar" % "1.6.4")
+
+javaOptions in run ++= Seq(
+  "-Djava.library.path=./sigar",
+  "-Xms128m", "-Xmx1024m",
+  "-Dcom.sun.management.jmxremote.port=9999",
+  "-Dcom.sun.management.jmxremote.authenticate=false",
+  "-Dcom.sun.management.jmxremote.ssl=false",
+  "-XX:+UnlockCommercialFeatures",
+  "-XX:+FlightRecorder")
+
+assemblyJarName in assembly := "SDAJClusterNode.jar"
+
+test in assembly := {}
+
+mainClass in assembly := Some("it.unipd.trluca.arsort.Main")
---
+++ src/main/scala/it/unipd/trluca/bsp/Main.scala
@@ -0,0 +1,60 @@
+package it.unipd.trluca.bsp
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+import scopt.OptionParser
+
+case class Config(debug:Boolean = false,
+                  clusterSize: Int = -1,
+//                  arraySize: Int = 10,
+//                  valueRange:Int = 10000,
+                  nodes: Seq[String] = Seq())
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val parser = new OptionParser[Config]("scopt") {
+      head("Distributed-Array Sorting", "1.0")
+      opt[Unit]('d', "debug") optional() action { (x, c) =>
+        c.copy(debug = true)
+      } text "Debug"
+      opt[Int]('c', "cSize") action { (x, c) =>
+        c.copy(clusterSize = x)
+      } text "Initial Cluster Size"
+//      opt[Int]('a', "arraySize") action { (x, c) =>
+//        c.copy(arraySize = x)
+//      } text "Distributed Array Size"
+//      opt[Int]('r', "valueRange") optional() action { (x, c) =>
+//        c.copy(valueRange = x)
+//      } text "Values range"
+
+      help("help") text "prints this usage text"
+
+      arg[String]("<host:port>...") unbounded() action { (x, c) =>
+        c.copy(nodes = c.nodes :+ x) } text "Nodes"
+    }
+
+    parser.parse(args, Config()) map { c =>
+
+      c.nodes foreach { h=>
+        val Array(hostname, port) = h.split(":")
+        val conf = ConfigFactory.parseString(s"akka.remote.netty.tcp.hostname=$hostname")
+          .withFallback(ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port"))
+          .withFallback(
+            if(c.debug)
+              ConfigFactory.load("app_debug")
+            else
+              ConfigFactory.load()
+          )
+//        val sys = ActorSystem("ClusterSystem", conf)
+//        sys.actorOf(Props[DistArrayNodeActor], "ablock")
+//
+//        if ((c.debug && port == "2551") || (!c.debug && c.arraySize != -1)) {
+//          val ep = sys.actorOf(Props[EntryPoint], "ep")
+//          ep ! SetDistArraySize(c)
+//        }
+      }
+    } //getOrElse {}
+
+  }
+}
